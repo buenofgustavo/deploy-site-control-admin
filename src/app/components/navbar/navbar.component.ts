2682e145c774bd150237efcd469bca51a377dff8
buenofgustavo <156470554+buenofgustavo@users.noreply.github.com>
2024-06-08 18:39:33 -0300

    Update navbar.component.ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -21,48 +21,44 @@ export class NavbarComponent {
       icon: 'home-outline',
       link: '/home',
     },
-    {
-      title: 'Departamento Pessoal',
-      icon: '',
-      hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_DP']),
-      children: [
-        {title: 'Gestão Pessoal',icon: '',link: '/gestao-pessoal'},
-        {title: 'Solicitaçoes Colaboradores',icon: '',link: '/solicitacao-colaboradores-dp',},
-        {title: 'Colaboradores',icon: '',link: '/colaboradores-dp',},
-      ]
-    },
+    //{
+      //title: 'Departamento Pessoal',
+      //icon: '',
+      //hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_DP']),
+      //children: [
+        //{title: 'Gestão Pessoal',icon: '',link: '/gestao-pessoal'},
+        //{title: 'Solicitaçoes Colaboradores',icon: '',link: '/solicitacao-colaboradores-dp',},
+        //{title: 'Colaboradores',icon: '',link: '/colaboradores-dp',},
+      //]
+    //},
 
     {
       title: 'Departamento T.I',
       icon: '',
       hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_TI']),
       children: [
-        {title: 'Solicitações Colaboradores',icon: '',link: '/solicitacoes-colaboradores-ti'},
-        // {title: 'Chamados',icon: '',link: '/chamados-ti'},
-        {title: 'Gestão Pessoal',icon: '',link: '/gestao-pessoal-ti'},
-        {title: 'Colaboradores',icon: '',link: '/colaboradores-ti',},
-        {title: 'Computadores',icon: '',link: '/computadores-ti',},
-        {title: 'Gestão de Ativos',
-        icon: '',
-        children: [
-          {title: 'Impressoras',icon: '',link: '/gestao-impressoras-ti'},
-          {title: 'Computadores Desativados',icon: '',link: '/gestao-computador-ti'},
-          {title: 'CPD',icon: '',link: '/gestao-cpd-ti'},
-          {title: 'Monitores',icon: '',link: '/gestao-monitor-ti'},
-          {title: 'Outros',icon: '',link: '/gestao-outros-ti'},
-        ]
+        //{title: 'Solicitações Colaboradores',icon: '',link: '/solicitacoes-colaboradores-ti'},
+         {title: 'Chamados',icon: '',link: '/chamados-ti'},
+        //{title: 'Gestão Pessoal',icon: '',link: '/gestao-pessoal-ti'},
+        //{title: 'Colaboradores',icon: '',link: '/colaboradores-ti',},
+        //{title: 'Computadores',icon: '',link: '/computadores-ti',},
+        //{title: 'Gestão de Ativos',
+        //icon: '',
+        //children: [
+        //  {title: 'Impressoras',icon: '',link: '/gestao-impressoras-ti'},
+        //  {title: 'Computadores Desativados',icon: '',link: '/gestao-computador-ti'},
+        //  {title: 'CPD',icon: '',link: '/gestao-cpd-ti'},
+        //  {title: 'Monitores',icon: '',link: '/gestao-monitor-ti'},
+        //  {title: 'Outros',icon: '',link: '/gestao-outros-ti'},
+        //]
       },
       ]
     },
     
-    // {
-    //   title: 'Chamados',
-    //   icon: '',
-    //   children: [
-    //     {title: 'Criar Chamado',icon: '',link: '/criar-chamados-geral'},
-    //     {title: 'Visualizar Chamados',icon: '',link: '/visualizar-chamados-geral'},
-    //   ]
-    // },
+
+         {title: 'Criar Chamado',icon: '',link: '/criar-chamados-geral'},
+         {title: 'Visualizar Chamados',icon: '',link: '/visualizar-chamados-geral'},
+
 
     {
       title: 'Cadastrar Usuários',
